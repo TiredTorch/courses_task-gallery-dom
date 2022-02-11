b54f4b08dd27abf5e41d2c17515b49cf9864f0ce
Tr.Torch <vitochek337@gmail.com>
2022-02-11 22:06:39 +0200

    Reacreated src taking system: now takes from parent href

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,13 @@
 'use strict';
 
-const pictures = [
-  './src/images/spring.jpg',
-  './src/images/summer.jpg',
-  './src/images/authum.jpg',
-  './src/images/winter.jpg'
-]
-
-const thumbs = document.querySelector('#thumbs');
 const mainImage = document.querySelector('#largeImg');
-const banners = document.getElementsByClassName('list-item__link');
-
 
 thumbs.addEventListener('click', (e) => {
   e.preventDefault();
   const currentElement = e.target.closest('a');
 
   if (currentElement) {
-    mainImage.src = pictures[[...banners].indexOf(currentElement)];
+    mainImage.src = currentElement.href;
   }
 })
 
